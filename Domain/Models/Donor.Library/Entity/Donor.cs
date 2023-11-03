d13f08a69e40c898f915a6dae0f209d9b93c1f01
ApachUA <4971779@gmail.com>
2023-11-03 19:08:14 +0200

    Fix requirements

--- Domain/Models/Donor.Library/Entity/Donor.cs
+++ Domain/Models/Donor.Library/Entity/Donor.cs
@@ -10,21 +10,21 @@ namespace Donor_Library.Entity
     {
         public int Donor_ID { get; set; }
 
-        public string Surname { get; set; }
+        public required string Surname { get; set; }
 
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
-        public string Patronymic { get; set; }
+        public string? Patronymic { get; set; }
 
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
-        public string Username { get; set; }
+        public required string Username { get; set; }
 
-        public string Logo { get; set; }
+        public string? Logo { get; set; }
 
-        public string Login { get; set; }
+        public required string Login { get; set; }
 
-        public string Password { get; set; }
+        public required string Password { get; set; }
 
         public int Donor_Role_ID { get; set; }
 
