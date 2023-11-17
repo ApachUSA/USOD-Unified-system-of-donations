c39cfd1144ff6342208b418bddce9742dcc97680
ApachUA <4971779@gmail.com>
2023-11-17 19:11:16 +0200

    Created realtime models library

---
+++ Domain/Models/RealTime_Library/Project_Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTime_Library
+{
+	public class Project_Comment
+	{
+		public int Comment_ID { get; set; }
+
+		public required string Comment_Message { get; set; }
+
+		public int Project_ID { get; set; }
+
+		public int Donor_ID { get; set; }
+
+		public DateTime Comment_Date { get; set; }
+	}
+}
---
+++ Domain/Models/RealTime_Library/Subscription.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTime_Library
+{
+	public class Subscription
+	{
+		public int Subscription_ID { get; set; }
+
+		public int Donor_ID { get; set; }
+
+		public int Fund_ID { get; set; }
+	}
+}
