36c7426a325f4a99c38403efa20c5b60b9ba195e
ApachUA <4971779@gmail.com>
2023-11-17 19:13:26 +0200

    Developed IBaseRepository and implemented him. also created db context and configure tables

---
+++ Microservices/USOD.RealTimeAPI/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace USOD.RealTimeAPI.Repositories.Interfaces
+{
+	public interface IBaseRepository<T> where T : class
+	{
+		Task Create(T entity);
+
+		Task Create(List<T> entity);
+
+		Task Delete(T entity);
+
+		Task Delete(List<T> entity);
+
+		IQueryable<T> Get();
+
+		Task<T> Update(T entity);
+
+		Task<List<T>> Update(List<T> entity);
+	}
+}
---
+++ Microservices/USOD.RealTimeAPI/Repositories/RealTime_DB_Context.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RealTime_Library;
+
+namespace USOD.RealTimeAPI.Repositories
+{
+	public class RealTime_DB_Context : DbContext
+	{
+		public RealTime_DB_Context(DbContextOptions<RealTime_DB_Context> options) : base(options)
+		{
+			Database.EnsureCreated();
+		}
+
+		public DbSet<Project_Comment> Project_Comments { get; set; }
+		public DbSet<Subscription> Subscriptions { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Project_Comment>(builder =>
+			{
+				builder.ToTable("Project_Comments").HasKey(x => x.Comment_ID);
+				builder.Property(x => x.Comment_ID).ValueGeneratedOnAdd();
+			});
+
+			modelBuilder.Entity<Subscription>(builder =>
+			{
+				builder.ToTable("Subscriptions").HasKey(x => x.Subscription_ID);
+				builder.Property(x => x.Subscription_ID).ValueGeneratedOnAdd();
+			});
+		}
+	}
+}
