d3e5b0b34fcb7a49e081e8b629bb50b934effce5
ApachUA <4971779@gmail.com>
2023-11-17 19:18:14 +0200

    Developed two controllers with service logic

---
+++ Microservices/USOD.RealTimeAPI/Controllers/ProjectCommentController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using RealTime_Library;
+using USOD.RealTimeAPI.Hubs;
+using USOD.RealTimeAPI.Services.Interfaces;
+
+namespace USOD.RealTimeAPI.Controllers
+{
+	[Route("RealTimeApi/[controller]")]
+	[ApiController]
+	public class ProjectCommentController : ControllerBase
+	{
+		private readonly IProjectCommentService _projectCommentService;
+		private readonly IHubContext<CommentHub, ICommentClient> _hubContext;
+		private readonly ILogger<ProjectCommentController> _logger;
+
+		public ProjectCommentController(IProjectCommentService projectCommentService, IHubContext<CommentHub, ICommentClient> hubContext, ILogger<ProjectCommentController> logger)
+		{
+			_projectCommentService = projectCommentService;
+			_hubContext = hubContext;
+			_logger = logger;
+		}
+
+		[HttpGet("{project_id}")]
+		public async Task<IActionResult> Get(int project_id)
+		{
+			var projectComment = await _projectCommentService.GetAsync(project_id);
+
+			if (!projectComment.Any()) return NotFound();
+
+			return Ok(projectComment);
+		}
+
+		[HttpGet("GetById/{projectComment_id}")]
+		public async Task<IActionResult> GetById(int projectComment_id)
+		{
+			var projectComment = await _projectCommentService.GetByIdAsync(projectComment_id);
+
+			if (projectComment == null) return NotFound();
+
+			return Ok(projectComment);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Post([FromBody] Project_Comment projectComment)
+		{
+			try
+			{
+				await _projectCommentService.CreateAsync(projectComment);
+
+				await _hubContext.Clients.Group(projectComment.Project_ID.ToString()).ReceiveComment(projectComment);
+
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Exception: {ex}", ex.Message);
+				return BadRequest(ex.Message);
+			}
+		}
+
+		//[HttpPut]
+		//public async Task<IActionResult> Put([FromBody] Card_Image projectComment)
+		//{
+		//	try
+		//	{
+		//		await _projectCommentService.UpdateAsync(projectComment);
+		//		return Ok(projectComment);
+		//	}
+		//	catch (Exception ex)
+		//	{
+		//		_logger.LogError("Exception: {ex}", ex.Message);
+		//		return BadRequest(ex.Message);
+		//	}
+		//}
+
+		[HttpDelete("{projectComment_id}")]
+		public async Task<IActionResult> Delete(int projectComment_id)
+		{
+			var projectComment = await _projectCommentService.GetByIdAsync(projectComment_id);
+			if (projectComment == null) return NotFound();
+
+			try
+			{
+				await _projectCommentService.DeleteAsync(projectComment);
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Exception: {ex}", ex.Message);
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
---
+++ Microservices/USOD.RealTimeAPI/Controllers/SubscriptionController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using RealTime_Library;
+using USOD.RealTimeAPI.Hubs;
+using USOD.RealTimeAPI.Services.Interfaces;
+
+namespace USOD.RealTimeAPI.Controllers
+{
+	[Route("RealTimeApi/[controller]")]
+	[ApiController]
+	public class SubscriptionController : ControllerBase
+	{
+		private readonly ISubscriptionService _subscriptionService;
+		private readonly IHubContext<SubscriptionHub, ISubClient> _hubContext;
+		private readonly ILogger<SubscriptionController> _logger;
+
+		public SubscriptionController(ISubscriptionService subService, IHubContext<SubscriptionHub, ISubClient> hubContext, ILogger<SubscriptionController> logger)
+		{
+			_subscriptionService = subService;
+			_hubContext = hubContext;
+			_logger = logger;
+		}
+
+		[HttpGet("{donor_id}")]
+		public async Task<IActionResult> Get(int donor_id)
+		{
+			var sub = await _subscriptionService.GetAsync(donor_id);
+
+			if (!sub.Any()) return NotFound();
+
+			return Ok(sub);
+		}
+
+		[HttpGet("GetById/{sub_id}")]
+		public async Task<IActionResult> GetById(int sub_id)
+		{
+			var sub = await _subscriptionService.GetByIdAsync(sub_id);
+
+			if (sub == null) return NotFound();
+
+			return Ok(sub);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Post([FromBody] Subscription sub)
+		{
+			try
+			{
+				await _subscriptionService.SubscribeAsync(sub);
+				await _hubContext.Clients.Group(sub.Fund_ID.ToString()).ReceiveMessage("Add new project");
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Exception: {ex}", ex.Message);
+				return BadRequest(ex.Message);
+			}
+		}
+
+		[HttpDelete("{sub_id}")]
+		public async Task<IActionResult> Delete(int sub_id)
+		{
+			var sub = await _subscriptionService.GetByIdAsync(sub_id);
+			if (sub == null) return NotFound();
+
+			try
+			{
+				await _subscriptionService.UnsubscribeAsync(sub);
+				return Ok();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Exception: {ex}", ex.Message);
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
