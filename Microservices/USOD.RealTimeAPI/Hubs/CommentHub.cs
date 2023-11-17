702c462342506c8476029853fcaa6f1311a6c600
ApachUA <4971779@gmail.com>
2023-11-17 19:17:42 +0200

    Created two strongly-typed hub and realised logic

---
+++ Microservices/USOD.RealTimeAPI/Hubs/CommentHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealTime_Library;
+using USOD.RealTimeAPI.Services.Interfaces;
+
+namespace USOD.RealTimeAPI.Hubs
+{
+	public class CommentHub : Hub<ICommentClient>
+	{
+		public async Task JoinProject(string project_id)
+		{
+			await Groups.AddToGroupAsync(Context.ConnectionId, project_id);
+		}
+			
+		public async Task SendMessage(Project_Comment comment)
+		{
+			await Clients.All.ReceiveMessage($"{Context.ConnectionId}: {comment}");
+		}
+	}
+
+	public interface ICommentClient
+	{
+		Task ReceiveMessage(string message);
+
+		Task ReceiveComment(Project_Comment project_Comment);
+	}
+}
---
+++ Microservices/USOD.RealTimeAPI/Hubs/SubscriptionHub.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealTime_Library;
+using USOD.RealTimeAPI.Services.Interfaces;
+
+namespace USOD.RealTimeAPI.Hubs
+{
+	public class SubscriptionHub : Hub<ISubClient>
+	{
+		private readonly ISubscriptionService _subscriptionService;
+		private readonly ILogger<SubscriptionHub> _logger;
+
+		public SubscriptionHub(ISubscriptionService subscriptionService, ILogger<SubscriptionHub> logger)
+		{
+			_subscriptionService = subscriptionService;
+			_logger = logger;
+		}
+
+		public async Task ConnectFund(int donor_id)
+		{
+			var subs = await _subscriptionService.GetAsync(donor_id);
+			foreach (var subscription in subs)
+			{
+				await Groups.AddToGroupAsync(Context.ConnectionId, subscription.Fund_ID.ToString());	
+			}	
+		}
+
+		public async Task Subscribe(Subscription subscription)
+		{
+			try
+			{
+				await _subscriptionService.SubscribeAsync(subscription);
+				await Groups.AddToGroupAsync(Context.ConnectionId, subscription.Fund_ID.ToString());
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("Exception: {ex}", ex.Message);
+			}
+			
+		}
+
+		public async Task Unsubscribe(int sub_id)
+		{
+			var sub = await _subscriptionService.GetByIdAsync(sub_id);
+			if(sub != null)
+			{
+				try
+				{
+					await _subscriptionService.UnsubscribeAsync(sub);
+					await Groups.RemoveFromGroupAsync(Context.ConnectionId, sub.Fund_ID.ToString());
+				}
+				catch (Exception ex)
+				{
+					_logger.LogError("Exception: {ex}", ex.Message);
+				}
+			}
+		}
+	}
+
+	public interface ISubClient
+	{
+		Task ReceiveMessage(string message);
+	}
+}
