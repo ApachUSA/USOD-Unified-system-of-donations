e70a3a1de6e2fa1ad3202e872526373159f8453a
ApachUA <4971779@gmail.com>
2023-11-22 15:41:01 +0200

    Added automapper for donor api

---
+++ Microservices/USOD.DonorAPI/Services/Mapping/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Donor_Library.Entity;
+using Donor_Library.ViewModel;
+
+namespace USOD.DonorAPI.Services.Mapping
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile() 
+		{
+			CreateMap<Donor, DonorVM>()
+				.ForMember(opt => opt.Donor_ID, opt => opt.MapFrom(src => src.Donor_ID))
+				.ForMember(opt => opt.Surname, opt => opt.MapFrom(src => src.Surname))
+				.ForMember(opt => opt.Name, opt => opt.MapFrom(src => src.Name))
+				.ForMember(opt => opt.Patronymic, opt => opt.MapFrom(src => src.Patronymic))
+				.ForMember(opt => opt.Email, opt => opt.MapFrom(src => src.Email))
+				.ForMember(opt => opt.Username, opt => opt.MapFrom(src => src.Username))
+				.ForMember(opt => opt.Logo, opt => opt.MapFrom(src => src.Logo));
+
+		}
+	}
+}
