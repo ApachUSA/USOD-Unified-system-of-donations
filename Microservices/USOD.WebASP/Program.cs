d43aec1736c9df14670b7bbe5fb8fe57efebef23
ApachUA <4971779@gmail.com>
2023-11-22 02:19:58 +0200

    Created HttpClientFactory, added cookie, registered donor service

--- Microservices/USOD.WebASP/Program.cs
+++ Microservices/USOD.WebASP/Program.cs
@@ -1,7 +1,28 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using USOD.WebASP.Services.Implementations;
+using USOD.WebASP.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddJsonOptions(x => x.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddHttpClient("Donor", client => {
+	client.BaseAddress = new Uri("http://localhost:5133/DonorApi/");
+});
+builder.Services.AddHttpClient("Fund", client => {
+	client.BaseAddress = new Uri("http://localhost:5133/FundApi/");
+});
+
+
+builder.Services.AddScoped<IDonorService, DonorService>();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddCookie(options =>
+	{
+		options.LoginPath = new PathString("/Donor/Login");
+		options.AccessDeniedPath = new PathString("/Donor/Login");
+	});
 
 var app = builder.Build();
 
