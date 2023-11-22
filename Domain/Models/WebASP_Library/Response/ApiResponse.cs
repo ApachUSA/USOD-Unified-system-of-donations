64e815a24c4645b535abb71c9bd94d34173c5f8d
ApachUA <4971779@gmail.com>
2023-11-22 02:17:26 +0200

    Created two response model

---
+++ Domain/Models/WebASP_Library/Response/ApiResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WebASP_Library.Response
+{
+	public static class ApiResponse
+	{
+		public static async Task<BaseResponse<T>> ProcessApiResponse<T>(HttpResponseMessage response)
+		{
+			if (response.StatusCode == System.Net.HttpStatusCode.OK)
+			{
+				var value = await JsonSerializer.DeserializeAsync<T>(await response.Content.ReadAsStreamAsync());
+				return BaseResponse<T>.Success(value);
+			}
+			else
+			{
+				return BaseResponse<T>.Fail(response.StatusCode, await response.Content.ReadAsStringAsync());
+			}
+		}
+	}
+}
---
+++ Domain/Models/WebASP_Library/Response/BaseResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebASP_Library.Response
+{
+	public class BaseResponse<T>
+	{
+		public string Description { get; set; } = string.Empty;
+		public HttpStatusCode StatusCode { get; set; }
+		public T Data { get; set; }
+
+		public static BaseResponse<T> Success(T? data, string description = "")
+		{
+			return new BaseResponse<T> { StatusCode = HttpStatusCode.OK, Data = data, Description = description };
+		}
+
+		public static BaseResponse<T> Fail(HttpStatusCode responseStatus, string description)
+		{
+			return new BaseResponse<T> { StatusCode = responseStatus, Description = description };
+		}
+
+		public static BaseResponse<T> Error(string description)
+		{
+			return new BaseResponse<T> { StatusCode = HttpStatusCode.InternalServerError, Description = description };
+		}
+	}
+}
