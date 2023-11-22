da21180f828a31677b312180784bfe222a763d05
ApachUA <4971779@gmail.com>
2023-11-22 15:41:57 +0200

    Created donor vm

---
+++ Domain/Models/Donor.Library/ViewModel/DonorVM.cs
@@ -0,0 +1,26 @@
+﻿using Donor_Library.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donor_Library.ViewModel
+{
+	public class DonorVM
+	{
+		public int Donor_ID { get; set; }
+
+		public required string Surname { get; set; }
+
+		public required string Name { get; set; }
+
+		public string? Patronymic { get; set; }
+
+		public required string Email { get; set; }
+
+		public required string Username { get; set; }
+
+		public string? Logo { get; set; }
+	}
+}
