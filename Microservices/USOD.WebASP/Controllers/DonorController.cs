ced7d8c2c35e2ff329aba715ec9c989903e6387c
ApachUA <4971779@gmail.com>
2023-11-24 19:20:11 +0200

    Implemented some methods and fixes

--- Microservices/USOD.WebASP/Controllers/DonorController.cs
+++ Microservices/USOD.WebASP/Controllers/DonorController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using System.Drawing;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using USOD.WebASP.Services.Interfaces;
 using WebASP_Library.Response;
@@ -31,28 +32,39 @@ namespace USOD.WebASP.Controllers
 			throw new Exception(response.Description);
 		}
 
-		//[HttpGet]
-		//public IActionResult Login() => View();
-
 		[HttpGet]
-		public async Task<IActionResult> Login()
+		public IActionResult Login() => View();
+
+		[HttpPost]
+		public async Task<IActionResult> Login(Donor_LoginVM donor_LoginVM)
 		{
 			if (ModelState.IsValid)
 			{
-				var response = await _donorService.Login(new Donor_LoginVM() {  Login = "admin", Password = "admin"});
+				var response = await _donorService.Login(donor_LoginVM);
 				if (response.StatusCode == System.Net.HttpStatusCode.OK)
 				{
-					List<Claim> claims = new()
-					{
-						new Claim(ClaimTypes.Name, "admin")
-					};
-					await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new( new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType)));
-					Response.Cookies.Append("Authorization", response.Data);
+					var handler = new JwtSecurityTokenHandler();
+					var jsonToken = handler.ReadToken(response.Data) as JwtSecurityToken;
+
+					//var name = jsonToken?.Claims.Where(x => x.Type == ClaimTypes.Name).Select(x => x.Value).FirstOrDefault();
+					//var role = jsonToken?.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value).FirstOrDefault();
+
+					//List<Claim> claims = new();
+
+					//if(name != null) claims.Add(new Claim(ClaimTypes.Name, name));
+					//if(role != null) claims.Add(new Claim(ClaimTypes.Role, role));
+
+					await HttpContext.SignInAsync(
+						CookieAuthenticationDefaults.AuthenticationScheme,
+						new(new ClaimsIdentity(jsonToken?.Claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType))
+						);
+
+					Response.Cookies.Append("Authorization", response.Data, new CookieOptions() { HttpOnly = true, SameSite = SameSiteMode.Strict });
 					return RedirectToAction("Index", "Home");
 				}
 				ModelState.AddModelError("", response.Description);
 			}
-			return View();
+			return View(donor_LoginVM);
 		}
 
 		[HttpGet]
@@ -61,32 +73,49 @@ namespace USOD.WebASP.Controllers
 		[HttpPost]
 		public async Task<IActionResult> Register(Donor donor)
 		{
-			var response = await _donorService.Register(donor);
+			if (ModelState.IsValid)
+			{
+				donor.Donor_Role_ID = 1;
+				var response = await _donorService.Register(donor);
+				if (response.StatusCode == System.Net.HttpStatusCode.OK)
+				{
+					return RedirectToAction("Login", "Donor", new { donor_LoginVM  = new Donor_LoginVM() {Login = donor.Login, Password = donor.Password } });
+				}
+				ModelState.AddModelError("", response.Description);
+			}
+			return View(donor);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Profile()
+		{
+			var response = await _donorService.GetProfile(int.Parse(HttpContext.User.FindFirst("Id").Value));
 			if (response.StatusCode == System.Net.HttpStatusCode.OK)
 			{
 				return View(response.Data);
+			}
+			throw new Exception(response.Description);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Profile(Donor donor)
+		{
+			var response = await _donorService.Update(donor);
+			if (response.StatusCode == System.Net.HttpStatusCode.OK)
+			{
+				return RedirectToAction("Profile");
 			}
 			throw new Exception(response.Description);
 		}
 
 		[HttpGet]
-		public async Task<IActionResult> Profile(int id)
+		public async Task<List<DonorVM>> GetInfo()
 		{
-			var response = await _donorService.GetById(id);
+			int[] mas = new int[] { 1, 12 };
+			var response = await _donorService.GetInfo(mas);
 			if (response.StatusCode == System.Net.HttpStatusCode.OK)
 			{
-				return View(response.Data);
-			}
-			throw new Exception(response.Description);
-		}
-
-		[HttpPut]
-		public async Task<IActionResult> UpdateProfile(Donor donor)
-		{
-			var response = await _donorService.Update(donor);
-			if (response.StatusCode == System.Net.HttpStatusCode.OK)
-			{
-				return View(response.Data);
+				return response.Data;
 			}
 			throw new Exception(response.Description);
 		}
@@ -100,6 +129,13 @@ namespace USOD.WebASP.Controllers
 				return View(response.Data);
 			}
 			throw new Exception(response.Description);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> LogOut()
+		{
+			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
