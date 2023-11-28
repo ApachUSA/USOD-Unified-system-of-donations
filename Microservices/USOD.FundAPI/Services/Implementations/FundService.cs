624f04194203b4d1aad074507dba4c473f2bbe2f
ApachUA <4971779@gmail.com>
2023-11-28 13:09:56 +0200

    The logic for creating media with the fund has been developed.

--- Microservices/USOD.FundAPI/Services/Implementations/FundService.cs
+++ Microservices/USOD.FundAPI/Services/Implementations/FundService.cs
@@ -8,15 +8,28 @@ namespace USOD.FundAPI.Services.Implementations
 	public class FundService : IFundService
 	{
 		private readonly IBaseRepository<Fund> _fundRepository;
+		private readonly IFundMediaService _fundMediaService;
 
-		public FundService(IBaseRepository<Fund> fundRepository)
+		public FundService(IBaseRepository<Fund> fundRepository, IFundMediaService fundMediaService)
 		{
 			_fundRepository = fundRepository;
+			_fundMediaService = fundMediaService;
 		}
 
 		public async Task<Fund> CreateAsync(Fund fund)
 		{
 			await _fundRepository.Create(fund);
+
+			List<Fund_Media> medias = new()
+			{
+				new Fund_Media { Fund_ID = fund.Fund_ID, Media_Type_ID = 1},
+				new Fund_Media { Fund_ID = fund.Fund_ID, Media_Type_ID = 2},
+				new Fund_Media { Fund_ID = fund.Fund_ID, Media_Type_ID = 3},
+				new Fund_Media { Fund_ID = fund.Fund_ID, Media_Type_ID = 4}
+			};
+
+			await _fundMediaService.CreateAsync(medias);
+
 			return fund;
 		}
 
@@ -28,7 +41,12 @@ namespace USOD.FundAPI.Services.Implementations
 
 		public async Task<List<Fund>> GetAsync()
 		{
-			return await _fundRepository.Get().ToListAsync();
+			return await _fundRepository.Get()
+				.Include(x => x.Fund_Medias)
+				.ThenInclude(x => x.Media_Type)
+				.Include(x => x.Fund_Members)
+				.ThenInclude(x => x.Member_Role)
+				.ToListAsync();
 		}
 
 		public async Task<Fund?> GetByIdAsync(int fund_id)
@@ -36,7 +54,9 @@ namespace USOD.FundAPI.Services.Implementations
 			return await _fundRepository.Get()
 				.Include(x => x.Fund_Images)
 				.Include(x => x.Fund_Medias)
+				.ThenInclude(x => x.Media_Type)
 				.Include(x => x.Fund_Members)
+				.ThenInclude(x => x.Member_Role)
 				.FirstOrDefaultAsync(x => x.Fund_ID == fund_id);
 		}
 
