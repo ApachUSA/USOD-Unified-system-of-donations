93df45c6c9e2540f08bd5180f56384ce2d1ea2f0
ApachUA <4971779@gmail.com>
2023-11-28 13:05:56 +0200

    Created fund vm

---
+++ Domain/Models/WebASP_Library/ViewModel/FundVM.cs
@@ -0,0 +1,18 @@
+﻿using Donor_Library.Entity;
+using Donor_Library.ViewModel;
+using Fund_Library.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebASP_Library.ViewModel
+{
+	public class FundVM
+	{
+		public List<DonorVM>? Fund_Members { get; set; }
+
+		public required Fund Fund { get; set; }
+	}
+}
