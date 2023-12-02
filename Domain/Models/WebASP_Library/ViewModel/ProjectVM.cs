a6db12caccee3c827b66d8855779de284f69720f
ApachUA <4971779@gmail.com>
2023-12-02 15:45:57 +0200

    Developed vm for project

---
+++ Domain/Models/WebASP_Library/ViewModel/ProjectVM.cs
@@ -0,0 +1,19 @@
+﻿using Fund_Library.Entity;
+using Project_Library.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebASP_Library.ViewModel
+{
+	public class ProjectVM
+	{
+		public required Project Project { get; set; }
+
+		public Fund? FundOwner { get; set; }
+
+		public List<Fund>? FundCoop { get; set; }
+	}
+}
