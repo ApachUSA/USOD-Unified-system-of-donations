d2c9d66eb6359279f7acc4fbedef08e8e8f9fca3
ApachUA <4971779@gmail.com>
2023-12-05 19:48:06 +0200

    left only the index method and the error

--- Microservices/USOD.WebASP/Controllers/HomeController.cs
+++ Microservices/USOD.WebASP/Controllers/HomeController.cs
@@ -1,27 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Donor_Library.Entity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json;
 using USOD.WebASP.Models;
+using USOD.WebASP.Services.Interfaces;
 
 namespace USOD.WebASP.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
-
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
-
-		public IActionResult Index()
-		{
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+		public IActionResult Index() => View();
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
