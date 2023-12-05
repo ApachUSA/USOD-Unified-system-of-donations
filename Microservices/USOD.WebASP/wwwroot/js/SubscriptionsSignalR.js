6281429ca730b47eb9b23c41b6b7c3d6db9152b5
ApachUA <4971779@gmail.com>
2023-12-05 12:53:59 +0200

    Created connecting to subscription hub script

---
+++ Microservices/USOD.WebASP/wwwroot/js/SubscriptionsSignalR.js
@@ -0,0 +1,20 @@
+const connectionToFund = new signalR.HubConnectionBuilder()
+	.withUrl("http://localhost:5261/subscription-hub")
+	.configureLogging(signalR.LogLevel.Information)
+	.build();
+
+async function start() {
+	try {
+		await connectionToFund.start();
+		console.log("SignalR Connected.");
+	} catch (err) {
+		console.log(err);
+		setTimeout(start, 5000);
+	}
+};
+
+connectionToFund.on("ReceiveMessage", function (message) {
+	ToastShow(message);
+});
+
+start();
