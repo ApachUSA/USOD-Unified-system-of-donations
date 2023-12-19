656167ae65820247390b82b6443fcfcdbd9ec55c
ApachUA <4971779@gmail.com>
2023-12-19 19:38:08 +0200

    Fixed some styles

--- Microservices/USOD.WebASP/wwwroot/js/ChangeAvatar.js
+++ Microservices/USOD.WebASP/wwwroot/js/ChangeAvatar.js
@@ -1,11 +1,8 @@
 function ChangeImage(ImageURL) {
 	var previewImage = document.getElementById("Donor_Avatar");
 
-	if (isValidURL(ImageURL)) {
 		previewImage.src = ImageURL;
-	} else {
-		previewImage.src = 'https://cdn-icons-png.flaticon.com/512/6614/6614285.png';
-	}
+
 }
 
 function isValidURL(url) {
